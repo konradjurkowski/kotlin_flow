5cd7ae6e80039f16c916ad3815b0ab33d6d3cd6d
konrad241198 <konrad.jurkowski11@gmail.com>
2021-07-06 18:57:29 +0200

    buffer a flow

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,24 +2,66 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun jediTrainess(): Flow<ForceUser> = forceUsers.asFlow()
+    .transform {
+        if (it is Padawan) {
+            delay(DELAY)
+            emit(it)
+        }
+    }
 
 fun main() = runBlocking {
-    fun duelOfTheFates(): Flow<ForceUser> = flow {
-        for (forceUser in forceUsers) {
-            delay(DELAY)
-            log("Battling ${forceUser.name}")
-            emit(forceUser)
-        }
-    }.transform { forceUser ->
-        if (forceUser is Sith) {
-            forceUser.name = "Darth ${forceUser.name}"
-        }
-        emit(forceUser)
-    }.flowOn(Dispatchers.Default)
 
-    duelOfTheFates().collect {
-        log("Battled ${it.name}")
+    var time = measureTimeMillis {
+        jediTrainess()
+            .collect {
+               delay(3 * DELAY)
+                log("Jedi ${it.name} is now a Jedi Master")
+            }
     }
+
+    log("Total time: $time ms")
+
+    exampleOf("buffer")
+
+    time = measureTimeMillis {
+        jediTrainess()
+            .buffer()
+            .collect {
+                delay(3 * DELAY)
+                log("Jedi ${it.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
+
+    exampleOf("conflate")
+
+    time = measureTimeMillis {
+        jediTrainess()
+            .conflate()
+            .collect {
+                delay(3 * DELAY)
+                log("Jedi ${it.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
+
+    exampleOf("collectLatest")
+
+    time = measureTimeMillis {
+        jediTrainess()
+            .collectLatest  {
+                log("Jedi Master training for ${it.name}")
+                delay(3 * DELAY)
+                log("Jedi ${it.name} is now a Jedi Master")
+            }
+    }
+
+    log("Total time: $time ms")
 }
 
 
