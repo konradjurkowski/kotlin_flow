f129f3e8228f938418fe151969b55efc5deb464c
konrad241198 <konrad.jurkowski11@gmail.com>
2021-07-06 10:46:46 +0200

    use flow builders

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,72 +1,21 @@
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
 
-    exampleOf("Sequence (blocks main thread)")
+    exampleOf("flowOf)")
 
-    fun prequels(): Sequence<String> = sequence {
-        for (movie in listOf(episodeI, episodeII, episodeIII)) {
-            Thread.sleep(DELAY)
-            yield(movie)
-        }
-    }
+    val favorites = flowOf(episodeV, episodeIX, episodeIV)
 
-    prequels().forEach { movie -> log(movie) }
+    favorites.collect { movie -> log(movie) }
 
-    log("Something else to be done here.")
+    exampleOf("asFlow")
 
-    exampleOf("Suspending function (asynchronous)")
+    val topAdjustedGrosses = listOf(episodeIV, episodeVII, episodeV)
 
-    suspend fun originals(): List<String> {
-        delay(DELAY)
-        return listOf(episodeIV, episodeV, episodeVI)
-    }
-
-    launch {
-        originals().forEach { movie -> log(movie) }
-    }
-
-    log("Something else to be done here.")
-
-    delay(2 * DELAY)
-    exampleOf("Simple flow")
-
-    fun sequels(): Flow<String> = flow {
-        for (movie in listOf(episodeVII, episodeVIII, episodeIX)) {
-            delay(DELAY)
-            emit(movie)
-        }
-    }
-
-    launch {
-        sequels().collect { movie -> log(movie) }
-    }
-
-    launch {
-        for (i in 1..3) {
-            log("Not blocked $i")
-            delay(DELAY)
-        }
-    }
-
-    log("Something else to be done here.")
-
-    delay(4 * DELAY)
-    exampleOf("Cold stream")
-
-    val sequelFilms = sequels()
-
-    sequelFilms.collect { movie -> log(movie) }
-
-    delay(3 * DELAY)
-    exampleOf("Collecting again")
-
-    sequelFilms.collect { movie -> log(movie) }
+    topAdjustedGrosses.asFlow().collect { movie -> log(movie) }
 }
 
 
